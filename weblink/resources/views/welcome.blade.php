f25478d3971dd2de10b9acb1238406ae19552cf9
Leonardo Abreu <tadeu1@live.com.pt>
2019-11-18 10:06:42 +0000

    welcome page almost done

--- weblink/resources/views/welcome.blade.php
+++ weblink/resources/views/welcome.blade.php
@@ -28,7 +28,20 @@
         <p class="textWC">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero suscipit et ipsam eos doloribus sunt quia in voluptates deserunt velit omnis eius porro fuga, iusto nam placeat rem. Possimus, aliquam?</p>
     </div>
     <div class="content">@yield('content')
-        
+        <div class="shortcutSection">
+            <div>
+                <img src="/img_welcome/coding.png" alt="">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
+            </div>
+            <div>
+                <img src="/img_welcome/coding.png" alt="">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
+            </div>
+            <div>
+                <img src="/img_welcome/coding.png" alt="">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
+            </div>
+        </div>
         <a href="#head"><img src="/icons/plus.svg" class="fixedbutton"></a>
     </div>
     <div class="footer">
