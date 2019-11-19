6e3bfa9f99076238f6b78abf60b21c734b12df5c
Leonardo Abreu <tadeu1@live.com.pt>
2019-11-19 11:35:00 +0000

    welcome page animation fix

--- weblink/resources/views/layout.blade.php
+++ weblink/resources/views/layout.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
 <body>
--- weblink/resources/views/welcome.blade.php
+++ weblink/resources/views/welcome.blade.php
@@ -10,23 +10,23 @@
         </div>
         
         <div class="shortcutSection">
-            <div>
-                <img src="/img_welcome/coding.png" alt="">
+            <div id="coding_div">
+                <img id="coding_img" src="/img_welcome/coding.svg" alt="">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
             </div>
-            <div>
-                <img src="/img_welcome/light.png" alt="">
+            <div id="idea_div">
+                <img id="idea_img" src="/img_welcome/light.svg" alt="">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
             </div>
-            <div>
-                <img src="/img_welcome/connection.png" alt="">
+            <div id="connection_div">
+                <img id="connection_img" src="/img_welcome/connection.svg" alt="">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
             </div>
             
         </div>
-        <div class="buttonDiv">
-        <button> START NOW</button> 
-        </div> 
+
+        <button id="startButton"> START NOW</button> 
+    <script src="/js/welcome.js"></script>
     </div>
     @endsection
 
