e16a478d7ea99f26e59d1c1afe3c56eede10f03d
Leonardo Abreu <tadeu1@live.com.pt>
2019-11-19 16:08:39 +0000

    merge conflict solution

--- weblink/resources/views/welcome.blade.php
+++ weblink/resources/views/welcome.blade.php
@@ -9,15 +9,7 @@
         <p class="textWC">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero suscipit et ipsam eos doloribus
             sunt quia in voluptates deserunt velit omnis eius porro fuga, iusto nam placeat rem. Possimus, aliquam?</p>
     </div>
-
-    <div class="shortcutSection">
-        <div>
-            <img src="/img_welcome/coding.png" alt="">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi
-                recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque
-                dolorem.</p>
-        </div>
-        
+       
         <div class="shortcutSection">
             <div id="coding_div">
                 <img id="coding_img" src="/img_welcome/coding.svg" alt="">
@@ -32,14 +24,11 @@
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium maxime distinctio nisi recusandae corporis similique fuga quisquam magnam aut quae natus quo, facilis sit nemo rerum ad cumque dolorem.</p>
             </div>
             
-        </div>
 
+        </div>
         <button id="startButton"> START NOW</button> 
     <script src="/js/welcome.js"></script>
-    </div>
-    <div class="buttonDiv">
-        <button> START NOW</button>
-    </div>
+    
 </div>
 @endsection
 
