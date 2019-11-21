eb64a23741df1f1fe3e5ac6d96e0769aaa606a10
Ruben Freitas <joseruben98@hotmail.com>
2019-11-21 18:45:05 +0000

    Profile fixed ellipsis

--- weblink/public/js/app.js
+++ weblink/public/js/app.js
@@ -49506,8 +49506,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/leonardoabreu/Desktop/WebLink/weblink/weblink/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/leonardoabreu/Desktop/WebLink/weblink/weblink/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/ruben/Projects/laravel/WebLink/weblink/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/ruben/Projects/laravel/WebLink/weblink/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
