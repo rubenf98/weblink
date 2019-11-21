4dc0a639f2bf5520a544b776cb95fb1dbef67c32
Ruben Freitas <joseruben98@hotmail.com>
2019-11-21 18:23:52 +0000

    Added nav bar icon

--- weblink/resources/views/layout/nav.blade.php
+++ weblink/resources/views/layout/nav.blade.php
@@ -128,8 +128,12 @@
 
     <div class="top-right links">
         @auth
+        <a href="{{ url('/profile/' . Auth::id()) }}"><img class="icon" src="/icons/user.svg"></a>
+
         <a href="{{ route('logout') }}" onclick="event.preventDefault();
         document.getElementById('logout-form').submit();"><img class="icon" src="/icons/logout.svg"></a>
+
+
 
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
