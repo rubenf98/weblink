1360da105a84efc4d9ef2324e8d0fd855afafa34
Leonardo Abreu <tadeu1@live.com.pt>
2019-11-25 19:35:43 +0000

    start a new profile layout

--- weblink/resources/views/layout.blade.php
+++ weblink/resources/views/layout.blade.php
@@ -58,7 +58,7 @@
     
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
-        modal.style.display = "block";
+         .style.display = "block";
     }
     
     // When the user clicks on <span> (x), close the modal
@@ -104,7 +104,7 @@
                         $('.chosen-select').trigger("chosen:updated");
                     });
                 });
-            }});
+                    }});
     });
     
        
